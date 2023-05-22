f783e65fc27d8785b2333c9a383a927b107bed35
Maan94919411* <intesaraljabri99@gmail.com>
2023-05-22 14:24:11 +0400

    Game_CreationSevice

--- src/main/java/Sports_Leaderboard/Sports_Leaderboard/Service/Game_CreationService.java
+++ src/main/java/Sports_Leaderboard/Sports_Leaderboard/Service/Game_CreationService.java
@@ -1,4 +1,17 @@
 package Sports_Leaderboard.Sports_Leaderboard.Service;
 
+import Sports_Leaderboard.Sports_Leaderboard.Models.Game_Creation;
+import Sports_Leaderboard.Sports_Leaderboard.Repositories.Game_CreationRepository;
+import antlr.collections.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class Game_CreationService {
+    @Autowired
+    Game_CreationRepository game_CreationRepository;
+
+    public List<Game_Creation> getAllGame_Creation(){
+        return game_CreationRepository.findAll();
+    }
 }
