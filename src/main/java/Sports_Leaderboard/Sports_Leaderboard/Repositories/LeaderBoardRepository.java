47553601a5c03d05655da6544eb2d88d593a3bdc
Maan94919411* <intesaraljabri99@gmail.com>
2023-05-22 20:02:17 +0400

    ledearboardrepository

--- src/main/java/Sports_Leaderboard/Sports_Leaderboard/Repositories/LeaderBoardRepository.java
+++ src/main/java/Sports_Leaderboard/Sports_Leaderboard/Repositories/LeaderBoardRepository.java
@@ -1,7 +1,10 @@
 package Sports_Leaderboard.Sports_Leaderboard.Repositories;
 
+import Sports_Leaderboard.Sports_Leaderboard.Models.LeaderBoard;
+import Sports_Leaderboard.Sports_Leaderboard.Models.Player;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LeaderBoardRepository {
+public interface LeaderBoardRepository extends JpaRepository<LeaderBoard, Integer> {
 }
