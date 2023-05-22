04cf44f2b7ab1688a6e79f3c21f0a1bdac04c92e
Maan94919411* <intesaraljabri99@gmail.com>
2023-05-22 19:57:20 +0400

    ledearboardservice

--- src/main/java/Sports_Leaderboard/Sports_Leaderboard/Service/LeaderBoardService.java
+++ src/main/java/Sports_Leaderboard/Sports_Leaderboard/Service/LeaderBoardService.java
@@ -1,4 +1,24 @@
 package Sports_Leaderboard.Sports_Leaderboard.Service;
 
+import Sports_Leaderboard.Sports_Leaderboard.Models.Game_Creation;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+@Service
 public class LeaderBoardService {
+
+    private List<Game_Creation> games = new ArrayList<>(); // In-memory storage for games
+
+    public void addGame(Game_Creation game) {
+        games.add(game);
+    }
+
+    public List<Game_Creation> getLeaderboard() {
+        // Sort the games by score in descending order
+        games.sort(Comparator.comparingInt(Game_Creation::getScore).reversed());
+        return games;
+    }
 }
