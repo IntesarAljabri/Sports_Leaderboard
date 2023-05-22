c99da895ae6a83e9fa95577667b5a5dad43fb978
Maan94919411* <intesaraljabri99@gmail.com>
2023-05-22 20:25:10 +0400

    GamemangmentService

--- src/main/java/Sports_Leaderboard/Sports_Leaderboard/Repositories/Game_MangementRepository.java
+++ src/main/java/Sports_Leaderboard/Sports_Leaderboard/Repositories/Game_MangementRepository.java
@@ -2,9 +2,14 @@ package Sports_Leaderboard.Sports_Leaderboard.Repositories;
 
 import Sports_Leaderboard.Sports_Leaderboard.Models.Game_Mangement;
 import Sports_Leaderboard.Sports_Leaderboard.Models.Player;
+import org.springframework.data.jpa.domain.AbstractPersistable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.metamodel.SingularAttribute;
+import java.io.Serializable;
+
 @Repository
 public interface Game_MangementRepository extends JpaRepository<Game_Mangement, Integer> {
+    void deleteById(SingularAttribute<AbstractPersistable, Serializable> id);
 }
--- src/main/java/Sports_Leaderboard/Sports_Leaderboard/Service/Game_MangementService.java
+++ src/main/java/Sports_Leaderboard/Sports_Leaderboard/Service/Game_MangementService.java
@@ -1,8 +1,13 @@
 package Sports_Leaderboard.Sports_Leaderboard.Service;
 
+import Sports_Leaderboard.Sports_Leaderboard.Models.Game_Mangement;
 import Sports_Leaderboard.Sports_Leaderboard.Repositories.Game_MangementRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 
 @Service
 public class Game_MangementService {
@@ -10,19 +15,19 @@ public class Game_MangementService {
     @Autowired
     Game_MangementRepository game_MangementRepository;
 
-    public Game saveGame(Game game) {
-        return gameRepository.save(game);
+    public Game_Mangement saveGame(Game_Mangement game) {
+        return game_MangementRepository.save(game);
     }
 
     public void deleteGame(Long gameId) {
-        gameRepository.deleteById(gameId);
+        game_MangementRepository.deleteById(id);
     }
 
-    public List<Game> getGamesByCreator(String creatorId) {
-        return gameRepository.findByCreatorId(creatorId);
+    public List<Game_Mangement> getGamesByCreator(String creatorId) {
+        return game_MangementRepository.findByCreatorId(creatorId);
     }
 
-    public List<Game> getAllGames() {
+    public List<Game_Mangement> getAllGames() {
         return gameRepository.findAll();
     }
 }
