885770922032c3a59041229411a86c2a42885276
Maan94919411* <intesaraljabri99@gmail.com>
2023-05-22 14:12:56 +0400

    Controller Classes

--- src/main/java/Sports_Leaderboard/Sports_Leaderboard/Controller/Game_CreationController.java
+++ src/main/java/Sports_Leaderboard/Sports_Leaderboard/Controller/Game_CreationController.java
@@ -1,26 +1,24 @@
 package Sports_Leaderboard.Sports_Leaderboard.Controller;
 
+import Sports_Leaderboard.Sports_Leaderboard.Models.Game_Creation;
+import antlr.collections.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/api/games")
 public class Game_CreationController {
-
-    private List<Game> games = new ArrayList<>(); // In-memory storage for games
+    private List games = (List<Game_Creation>) new ArrayList<>(); // In-memory storage for games
 
     @PostMapping
-    public ResponseEntity<String> createGame(@RequestBody Game game, Principal principal) {
-        if (!isAuthenticated(principal)) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Authentication failed");
-        }
-
-        if (game.getName() == null || game.getPlayers() == null || game.getScore() == 0) {
-            return ResponseEntity.badRequest().body("Missing required fields");
-        }
-
+    public ResponseEntity<String> createGame(@RequestBody Game_Creation game) {
         games.add(game);
 
         return ResponseEntity.status(HttpStatus.CREATED).body("Game created successfully");
