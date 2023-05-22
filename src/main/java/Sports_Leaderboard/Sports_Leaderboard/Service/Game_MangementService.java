a9cb6041ed82203e0a9937790b00eda8a08d462a
Maan94919411* <intesaraljabri99@gmail.com>
2023-05-22 20:14:35 +0400

    GamemangmentService

--- src/main/java/Sports_Leaderboard/Sports_Leaderboard/Service/Game_MangementService.java
+++ src/main/java/Sports_Leaderboard/Sports_Leaderboard/Service/Game_MangementService.java
@@ -1,7 +1,28 @@
 package Sports_Leaderboard.Sports_Leaderboard.Service;
 
+import Sports_Leaderboard.Sports_Leaderboard.Repositories.Game_MangementRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class Game_MangementService {
+
+    @Autowired
+    Game_MangementRepository game_MangementRepository;
+
+    public Game saveGame(Game game) {
+        return gameRepository.save(game);
+    }
+
+    public void deleteGame(Long gameId) {
+        gameRepository.deleteById(gameId);
+    }
+
+    public List<Game> getGamesByCreator(String creatorId) {
+        return gameRepository.findByCreatorId(creatorId);
+    }
+
+    public List<Game> getAllGames() {
+        return gameRepository.findAll();
+    }
 }
