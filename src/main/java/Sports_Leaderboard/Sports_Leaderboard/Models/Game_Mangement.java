bb45d820ff3a2dd515297a1eaf64c24b05559df7
Maan94919411* <intesaraljabri99@gmail.com>
2023-05-22 14:16:40 +0400

    GameMengement

--- src/main/java/Sports_Leaderboard/Sports_Leaderboard/Models/Game_Mangement.java
+++ src/main/java/Sports_Leaderboard/Sports_Leaderboard/Models/Game_Mangement.java
@@ -3,12 +3,20 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+
 @Data
 @Setter
 @Getter
 @Entity
 @Table(name = "Game_Mangement")
 public class Game_Mangement {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Integer id;
+
+
+    String GameName;
+    String GameInformation;
+    String Price;
 }
