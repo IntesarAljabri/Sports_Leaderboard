c84ed6f9dffb0f3aa58f4c17afee31b48e32f419
Maan94919411* <intesaraljabri99@gmail.com>
2023-05-22 14:18:21 +0400

    Game_MangementRepository

--- src/main/java/Sports_Leaderboard/Sports_Leaderboard/Repositories/Game_MangementRepository.java
+++ src/main/java/Sports_Leaderboard/Sports_Leaderboard/Repositories/Game_MangementRepository.java
@@ -1,7 +1,10 @@
 package Sports_Leaderboard.Sports_Leaderboard.Repositories;
 
+import Sports_Leaderboard.Sports_Leaderboard.Models.Game_Mangement;
+import Sports_Leaderboard.Sports_Leaderboard.Models.Player;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Game_MangementRepository {
+public interface Game_MangementRepository extends JpaRepository<Game_Mangement, Integer> {
 }
