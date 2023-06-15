fa6405d41749a4d84fd69336d65e4093f9388fb5
Maan94919411* <intesaraljabri99@gmail.com>
2023-06-15 14:51:40 +0400

    completed the part of Registration class in controller and service

--- src/main/java/Sports_Leaderboard/Sports_Leaderboard/Controller/RegistrationController.java
+++ src/main/java/Sports_Leaderboard/Sports_Leaderboard/Controller/RegistrationController.java
@@ -1,9 +1,10 @@
 package Sports_Leaderboard.Sports_Leaderboard.Controller;
 
+import Sports_Leaderboard.Sports_Leaderboard.Models.Registration;
 import Sports_Leaderboard.Sports_Leaderboard.Service.RegistrationService;
+import Sports_Leaderboard.Sports_Leaderboard.Service.TeamService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -11,6 +12,18 @@ public class RegistrationController {
 
     @Autowired
     RegistrationService registrationService;
+    @Autowired
+    TeamService teamService;
+    @GetMapping(value = "getRegistrationById")
+    public Registration getRegistrationById(@RequestParam Integer id) {
+        Registration registrationById = registrationService.getRegistrationById(id);
+        return registrationById;
+    }
+    @RequestMapping(value = "RegisterNewTeam", method = RequestMethod.POST)    //Register Player
+    public void registerNewTeam() {
+        teamService.createTeam();
+    }
+
 
 
 
--- src/main/java/Sports_Leaderboard/Sports_Leaderboard/Service/RegistrationService.java
+++ src/main/java/Sports_Leaderboard/Sports_Leaderboard/Service/RegistrationService.java
@@ -1,14 +1,33 @@
 package Sports_Leaderboard.Sports_Leaderboard.Service;
 
+import Sports_Leaderboard.Sports_Leaderboard.Models.Registration;
+import Sports_Leaderboard.Sports_Leaderboard.Models.Team;
 import Sports_Leaderboard.Sports_Leaderboard.Repositories.RegistrationRepository;
+import Sports_Leaderboard.Sports_Leaderboard.Request.RegistrationRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+
 @Service
 public class RegistrationService {
-
     @Autowired
     RegistrationRepository registrationRepository;
 
+    public void RegisterTeam(RegistrationRequest registrationRequest) {
+        Registration registration = new Registration();
+        registration.setUserName(registrationRequest.getUserName());
+        registration.setTeamName(registrationRequest.getTeamName());
+        registration.setPassword(registrationRequest.getPassword());
+        registration.setSportType(registrationRequest.getSportType());
+        registration.setCreateDate(new Date());
+        registration.setIsActive(true);
+        registrationRepository.save(registration);
+    }
+    public Registration getRegistrationById(Integer id) {
+        Registration registrationById = registrationRepository.getRegistrationId(id);
+        return registrationById;
+
+    }
 
 }
